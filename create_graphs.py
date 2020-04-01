87fe348e9581f043eedee3f01fcddd4657d3cdd3
Lubkdog <guilherme.lubk-do-prado@student-cs.fr>
2020-04-01 11:29:29 +0200

    Added a new way to choose the degree of each node in the test graphs, now incluing a Poisson Variable. Also, changed the way node connexions are atributted, in order to optimize the program for bigger graphs

--- create_graphs.py
+++ create_graphs.py
@@ -1,9 +1,20 @@
 import os
 import math
 
+#Parameters to be defined for each test
+#For comparison, the number of graphs shold be high and constant
 num_graphs = 1
-folder = 'tests/'
-base_name = 'test'
+size = 30
+dist = "poisson"
+lam = 15
+
+folder = "tests_n_" + str(size) + "_dist_" + dist
+if dist == "poisson":
+    folder = folder + "_lam_" + str(lam) + "/"
+else:
+    folder = folder + "/"
+
+base_name = "test"
 #defining number of digits for name encoding
 nDigits = math.trunc(math.log10(num_graphs)) + 1
 
@@ -12,13 +23,12 @@ if not os.path.exists(folder):
     os.makedirs(folder)
 
 
-size = 30
-dist = "poisson"
-
 print("Creating graphs\n")
 for i in range(num_graphs):
-    print("Created", str(i).zfill(nDigits), "of", num_graphs)
-    os.system("python3 generate_graph.py --fname " + folder + base_name + str(i).zfill(nDigits) + '.gm --size ' + str(size) + " --dist " + dist)
+    print("Created", str(i+1).zfill(nDigits), "of", num_graphs)
+    os.system("python3 generate_graph.py --fname " + folder + 
+        base_name + str(i).zfill(nDigits) + ".gm --size " + str(size) + " --dist " + dist
+        + " --lam " + str(lam))
 
 #File Structure
 print("\nFile Structure:")
--- generate_graph.py
+++ generate_graph.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import randrange, choice
 from uuid import uuid4
 from argparse import ArgumentParser
 import sys
@@ -11,49 +11,53 @@ def main(fname, size, dist, lam):
     with open(fname, 'w') as fout:
         #Each graph have the same amount of nodes 
         size = int(size) - 1
+        #Title of each test file
         fout.write("parity " + str(size+1) + ";\n")
+        
+        #Loop to create each node
         for i in  range(size+1):
+            #node_name
             fout.write(str(i) + ' ')
+            #color
             priority = randrange(1, 100)
             fout.write(str(priority) + ' ')
+            #player
             owner = randrange(0, 2)
             fout.write(str(owner) + ' ')
-            #Number of edges
-            if dist == "unif":
+            #Neighbours
+            #Number of edges of the this node
+            if dist == "unif": 
                 number_of_edges = randrange(1, size+1)
             elif dist == "poisson":
-                number_of_edges = np.random.poisson(lam) + 1
+                number_of_edges = np.random.poisson(int(lam)) + 1
                 if number_of_edges > size:
-                    number_of_edges = 4
-                print("Number of edges = " + str(number_of_edges))
-            elif dist == "gaussian":
+                    number_of_edges = size
+            elif dist == "heavy":
                 number_of_edges = 10
             
-            used = [0] * (size + 1)
+            #Defining connected nodes to node i
+            sequence = list(range(size+1))
+            #No self-loops allowed
+            sequence.remove(i)
             for j in range(number_of_edges):
-
-                while True:
-                    t = randrange(0, size + 1)
-                    if t != i and used[t] == 0:
-                        # only add edges to other vertexes, dont allow self loops
-                        used[t] = 1
-                        fout.write(str(t))
-                        break
-
+                t = choice(sequence)
+                fout.write(str(t))
+                sequence.remove(t)
                 if j == number_of_edges - 1:
                     fout.write(" ")
                 else:
                     fout.write(",")
 
-            
+            #Create a uuid for each node
             uuid = uuid4()
             fout.write("\"" + str(uuid) + "\";\n")
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--fname", help="name of the file to be created", nargs=1, required=True)
-    parser.add_argument("--size", help="max size of each graph. Default = 30", nargs=1, required=True)
-    parser.add_argument("--dist", help="unif - poisson - gaussian - probability distribution of the degree of each node", nargs=1, required=True)
+    parser.add_argument("--size", help="size of each graph. Default = 30", nargs=1, required=True)
+    parser.add_argument("--dist", help="unif/poisson/heavy -> probability distribution of the degree of each node", nargs=1, required=True)
+    parser.add_argument("--lam", help="value of lambda for poisson distribuitions", nargs=1, required=True)
 
     try:
         args = parser.parse_args()
@@ -61,5 +65,5 @@ if __name__ == '__main__':
         parser.print_help(sys.stderr)
         exit(1)
 
-    main(args.fname[0], args.size[0], args.dist[0], lam = 30)
+    main(args.fname[0], args.size[0], args.dist[0], args.lam[0])
 
