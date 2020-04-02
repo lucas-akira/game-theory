d3e4a7d193aef10df8d9dffc4124329ccaab1050
Gabriel Pellegrino da Silva <gpsunicamp016@gmail.com>
2020-04-02 16:37:37 +0200

    Lubkão added new features to create graphs: different distributions

--- create_graphs.py
+++ create_graphs.py
@@ -1,12 +1,36 @@
 import os
+import math
 
-num_graphs = 100
-folder = 'tests/'
-base_name = 'test'
+#Parameters to be defined for each test
+#For comparison, the number of graphs shold be high and constant
+num_graphs = 1
+size = 30
+dist = "poisson"
+lam = 15
+
+folder = "tests_n_" + str(size) + "_dist_" + dist
+if dist == "poisson":
+    folder = folder + "_lam_" + str(lam) + "/"
+else:
+    folder = folder + "/"
+
+base_name = "test"
+#defining number of digits for name encoding
+nDigits = math.trunc(math.log10(num_graphs)) + 1
+
 
 if not os.path.exists(folder):
     os.makedirs(folder)
 
+
+print("Creating graphs\n")
 for i in range(num_graphs):
-    print("Created", str(i).zfill(4), "of", num_graphs)
-    os.system("python3 generate_graph.py --fname " + folder + base_name + str(i).zfill(4) + '.gm')
+    print("Created", str(i+1).zfill(nDigits), "of", num_graphs)
+    os.system("python3 generate_graph.py --fname " + folder + 
+        base_name + str(i).zfill(nDigits) + ".gm --size " + str(size) + " --dist " + dist
+        + " --lam " + str(lam))
+
+#File Structure
+print("\nFile Structure:")
+print("Header: purpose number_of_nodes")
+print("Body: node_name color player neighbours id_code")
